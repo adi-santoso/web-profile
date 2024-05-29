e5ad5f7b5f1b28d61ea30966850f122a9b4db0fd
Adi Santoso <adhievista@gmail.com>
2024-05-29 20:25:11 +0700

    add escape special chart

--- src/app/(page)/home.tsx
+++ src/app/(page)/home.tsx
@@ -1,5 +1,3 @@
-import bgHome from '../public/bg_home.png';
-
 export default function HomePage() {
     return (
         // <div className="container h-screen place-content-center" style={{backgroundImage:'url("https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/01/satoru_gojo_jujutsu_kaisen_manga.jpg")', backgroundRepeat:'no-repeat'}}>
@@ -14,7 +12,7 @@ export default function HomePage() {
                 <tr>
                     <td width={'50%'} style={{padding:'100px', backgroundColor:"rgba(0,0,0,0.53)", height:"100%"}}>
                         <h1 style={{fontSize: '55px', textTransform: 'uppercase'}}>
-                            <b>Hi, I'm ADI!</b>
+                            <b>Hi, I&apos;m ADI!</b>
                             <br/><span><b className="me-5">Flutter</b></span>
                             <span style={{color: "#0CC0DF"}}><b className="is-visible">Developer</b></span>
                         </h1>
