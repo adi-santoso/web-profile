d2c616bbb9f078c84db8041242dae18193c77d6a
Adi Santoso <adhievista@gmail.com>
2024-06-24 13:46:24 +0700

    update navbar hover color same as active state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,31 +10,10 @@ import Contact from "@/app/(page)/contact";
 import Image from "next/image";
 
 export default function Page() {
-    const colors = [
-        "rgba(255,172,18,0.2)",
-        "rgb(119,225,109)",
-        "rgba(164,191,217,0.2)",
-        "rgba(95,194,194,0.2)",
-        "rgba(194,121,121,0.2)"
-    ];
-    const [index, alterIndex] = useState(0);
 
-    const nextPage = () => {
-        var newIndex = index+1;
-        if(newIndex>colors.length){
-            newIndex = 0;
-        }
-        alterIndex(newIndex);
-    };
-    const previousPage = () =>{
-        var newIndex = index-1;
-        if(newIndex<0){
-            newIndex = 0;
-        }
-        alterIndex(newIndex);
-    };
 
     const [activeNavbarIndex, changeActiveNavbar] = useState(0);
+    const [hoverNavbarIndex, changeHoverActiveNavbar] = useState(0);
 
     const navbarHandler = (e: any) =>
     {
@@ -46,11 +25,22 @@ export default function Page() {
     }
     const navBarActiveColor = "rgb(255,255,255)";
 
+    const navbarMouseInHandler = (event: any) =>
+    {
+        changeHoverActiveNavbar(event.target.getAttribute('data-index'));
+    }
+
+    const navbarMouseLeaveHandler = (event: any) =>
+    {
+        changeHoverActiveNavbar(-1);
+    }
+
+
     const navigation = [
-        { name: 'Home', href: '#home', color: activeNavbarIndex==0 ? navBarActiveColor : "", icon: <Home sx={{ fontSize: 40 }} color={activeNavbarIndex==0 ? "inherit" : undefined} className="me-2"/> },
-        { name: 'About Me', href: '#about', color: activeNavbarIndex==1 ? navBarActiveColor : "", icon: <AccountCircle sx={{ fontSize: 40 }} color={activeNavbarIndex==1 ? "inherit" : undefined}  className="me-2"/> },
-        { name: 'Projects', href: '#project', color: activeNavbarIndex==2 ? navBarActiveColor : "", icon: <FolderSpecial sx={{ fontSize: 40 }}  color={activeNavbarIndex==2 ? "inherit" : undefined} className="me-2"/> },
-        { name: 'Contact', href: '#contact', color: activeNavbarIndex==3 ? navBarActiveColor : "", icon: <ContactPhone sx={{ fontSize: 40 }} color={activeNavbarIndex==3 ? "inherit" : undefined}  className="me-2"/> },
+        { name: 'Home', href: '#home', color: activeNavbarIndex==0 || hoverNavbarIndex ==0 ? navBarActiveColor : "", icon: <Home sx={{ fontSize: 40 }} color={activeNavbarIndex==0 ? "inherit" : undefined} className="navbar-icon"/> },
+        { name: 'About Me', href: '#about', color: activeNavbarIndex==1 || hoverNavbarIndex ==1  ? navBarActiveColor : "", icon: <AccountCircle sx={{ fontSize: 40 }} color={activeNavbarIndex==1 ? "inherit" : undefined}  className="navbar-icon"/> },
+        { name: 'Projects', href: '#project', color: activeNavbarIndex==2 || hoverNavbarIndex ==2  ? navBarActiveColor : "", icon: <FolderSpecial sx={{ fontSize: 40 }}  color={activeNavbarIndex==2 ? "inherit" : undefined} className="navbar-icon"/> },
+        { name: 'Contact', href: '#contact', color: activeNavbarIndex==3 || hoverNavbarIndex ==3  ? navBarActiveColor : "", icon: <ContactPhone sx={{ fontSize: 40 }} color={activeNavbarIndex==3 ? "inherit" : undefined}  className="navbar-icon"/> },
     ]
 
     function classNames(...classes: string[]) {
@@ -67,21 +57,35 @@ export default function Page() {
                 <div className="" style={{backgroundColor:'rgba(96,96,96,0.32)'}}>
                     <Image src='/brand_me_white.png' className="mx-auto" width={150} height={150} alt={'brand'}></Image>
                 </div>
-                <ul className="">
+                <ul className="p-5">
                     {navigation.map((item, index) => (
-                        <li key={item.name} className="p-5 w-full inline-block">
+                        <li key={item.name} className="w-full inline-block" onMouseEnter={navbarMouseInHandler} onMouseLeave={navbarMouseLeaveHandler}>
                             <a
+
                                 key={item.name}
                                 href={item.href}
                                 className={classNames(
-                                        activeNavbarIndex == index ? 'navbar-active' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                    'rounded-md px-3 py-2 text-sm font-medium p-5 w-full block float-left'
+                                        activeNavbarIndex == index || hoverNavbarIndex == index ? 'navbar-active' : 'navbar-inactive',
+                                    'navbar'
                                 )}
-                                aria-current={activeNavbarIndex == index ? 'page' : undefined}
+                                aria-current={activeNavbarIndex == index || hoverNavbarIndex == index ? 'page' : undefined}
                             >
-                                {item.icon}
-                                <span className={'uppercase font-extrabold'}
-                                      style={{color: item.color}}>{item.name}</span>
+                                <div className={'flex'} style={{height:'60px'}}>
+                                    <div
+                                        data-index={index}
+                                        className={classNames(
+                                        activeNavbarIndex == index || hoverNavbarIndex == index ? 'navbar-icon-active' : 'navbar-icon-inactive',
+                                        'place-content-center text-center'
+                                    )} style={{width:'60px'}}>
+                                        {item.icon}
+                                    </div>
+                                    <div data-index={index} className={'flex-grow place-content-center'}>
+                                    <span data-index={index} className={'uppercase font-extrabold ms-2'} style={{color: item.color}}>
+                                    {item.name}
+                                </span>
+                                    </div>
+                                </div>
+
                             </a>
                         </li>
                     ))}
